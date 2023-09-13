2050b0e02a4bce6a34d4d23472ad10ca1e319ecd
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-09-13 23:40:48 +0100

    person controller

---
+++ Stage2/src/routes/userRoute.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  createPerson,
+  getPerson,
+  updatePerson,
+  deletePerson,
+} = require("../controllers/controller");
+
+//@description: create a person
+//@route: POST '/api'
+router.get("/api", createPerson);
+
+//@description: get person
+//@route: GET '/api/:user_id'
+router.get("/api/:user_id", getPerson);
+
+//@description: update a person
+//@route: POST '/api/:user_id'
+router.get("/api/:user_id", updatePerson);
+
+//@description: delete a person
+//@route: POST '/api/:user_id'
+router.get("/api/:user_id", deletePerson);
