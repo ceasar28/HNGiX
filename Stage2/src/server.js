f1e274e97882f779dbc18e6ef2ecfb74a4fa979e
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-09-14 00:31:46 +0100

    stage 2 task

--- Stage2/src/server.js
+++ Stage2/src/server.js
@@ -1,16 +1,34 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const PORT = process.env.PORT || 5000;
+require("dotenv").config();
+const PORT = 5000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const connectDb = require("../config/database");
 const route = require("./routes/route");
+const notFound = require("./middlewares/errorMiddleware");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors()); // to initialise the cors middleware
 
 app.use("/", route);
 app.use(notFound);
 
-app.listen(PORT, () => {
-  console.log(`app is listening on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`app is listening on ${PORT}`);
+// });
+
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_CONNECT);
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port : ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// start connection process
+start();
