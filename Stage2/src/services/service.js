9dd3562d7d0a28b55fd780fe6933bc43eb7babfd
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-09-13 22:36:16 +0100

    person service

--- Stage2/src/services/service.js
+++ Stage2/src/services/service.js
@@ -13,11 +13,77 @@ class PersonServices {
         const person = new Person({ name });
         const saved = await person.save();
         if (saved) {
-          return { success: "user saved succefull", data: saved };
+          return { success: "user saved successfull", data: saved };
         }
       }
     } catch (err) {
       return { error: err.message };
     }
   }
+
+  // read person
+  async getPerson(id) {
+    try {
+      const fetch = await Person.findById(id);
+      if (!fetch) {
+        const error = new Error("Person not found");
+        return { error: error };
+      } else {
+        return { success: "Person found", data: fetch };
+      }
+    } catch (err) {
+      // Check if the error is a CastError (invalid ID)
+      if (err.name === "CastError") {
+        return { error: "Invalid id" };
+      }
+      return { error: "Server failed to fetch user. Try again..." };
+    }
+  }
+
+  // update person
+  async updatePerson(data) {
+    const name = data.name;
+    const id = data.id;
+    try {
+      const updatedUser = await Person.findByIdAndUpdate(
+        id,
+        { name },
+        { new: true }
+      );
+      if (!updatedUser) {
+        const error = new Error("Person not found");
+        return { error: error };
+      } else {
+        return { success: "Person Updated Successfully", data: updatedUser };
+      }
+    } catch (err) {
+      // Check if the error is a CastError (invalid ID)
+      if (err.name === "CastError") {
+        return { error: "Invalid id" };
+      }
+      return { error: "failed to Update person. Try again..." };
+    }
+  }
+
+  // delete person
+  // read person
+  async deletePerson(id) {
+    try {
+      const deletedPerson = await Person.findByIdAndDelete(id);
+      if (!deletedPerson) {
+        const error = new Error("Person not found");
+        return { error: error };
+      } else {
+        return { success: "Person Deleted successfully", data: deletedPerson };
+      }
+    } catch (err) {
+      // Check if the error is a CastError (invalid ID)
+      if (err.name === "CastError") {
+        return { error: "Invalid id" };
+      }
+      return { error: " failed to delete user. Try again..." };
+    }
+  }
 }
+
+module.exports = new PersonServices();
