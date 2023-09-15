6f12a92e14a7b8a9bb78c677be50e235bb6adc49
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-09-15 08:08:21 +0100

    swagger error

--- Stage2/src/routes/personRoute.js
+++ Stage2/src/routes/personRoute.js
@@ -59,8 +59,29 @@ router.post("/api", createPerson);
  * @swagger
  * /users:
  *   get:
- *     summary: Retrieve a list of JSONPlaceholder users
+ *     summary: Retrieve a list of JSONPlaceholder users.
  *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         description: The user ID.
+ *                         example: 0
+ *                       name:
+ *                         type: string
+ *                         description: The user's name.
+ *                         example: Leanne Graham
  */
 
 //@description: get person
