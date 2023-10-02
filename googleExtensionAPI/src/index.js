d1a1157725859c3df3909203fc784a87bbd88822
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-10-02 11:01:23 +0100

    extension api first working version

--- googleExtensionAPI/src/index.js
+++ googleExtensionAPI/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import morgan from "morgan";
+// import morgan from "morgan";
 import router from "./routes/video.route.js";
 import errorHandler from "./middleware/errorHandler.js";
 
@@ -8,7 +8,7 @@ const app = express();
 
 // Third-party middlewares
 app.use(cors());
-app.use(morgan("dev"));
+//app.use(morgan("dev"));
 
 // In-built middlewares
 app.use(express.json({ limit: "50mb" }));
